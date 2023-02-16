b560a275ec391291545704caa83db7ea6ad5046e
magda <magdalena.lozowska@gmail.com>
2023-02-16 21:13:13 +0000

    cucumber tests

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,22 @@
 const { defineConfig } = require("cypress");
+const cucumber = require("cypress-cucumber-preprocessor").default;
 
 module.exports = defineConfig({
+  projectId: "s9obpt",
   e2e: {
+    specPattern: "cypress/e2e/**/*.{js,feature}",
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      on("file:preprocessor", cucumber());
     },
     baseUrl:
       "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login",
     watchForFileChanges: false,
+    chromeWebSecurity: false,
+    defaultCommandTimeout: 10000,
+    pageLoadTimeout: 120000,
+    screenshotOnRunFailure: true,
+    trashAssetsBeforeRuns: false,
+    video: false,
   },
 });
